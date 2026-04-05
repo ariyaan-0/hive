6062380e7c5ad653e87015da3e2b90e68595f168
Md Rezaul Hossain <str.rezaul@gmail.com>
2026-04-05 21:14:30 +0600

    add path operation for Posts

--- hive-backend/app/main.py
+++ hive-backend/app/main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import Depends, FastAPI, Body, status, HTTPException
+from fastapi import Depends, FastAPI, Body, Response, status, HTTPException
 from pydantic import BaseModel
 from . import models
 from .database import engine, get_db
@@ -17,7 +17,6 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
 
 
 # Route or Path operation
@@ -27,13 +26,53 @@ async def root():
 
 @app.get("/sqlalchemy")
 def test_posts(db: Session = Depends(get_db)):
-    return {"status": "Success"}
+    posts = db.query(models.Post).all()
+    return {"data": posts}
 
 @app.get("/posts")
-def get_posts():
-    return {"data": "This is your posts"}
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return {"data": posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(new_post : Post):
-    print(new_post.model_dump())
-    return {"message": "new post has been created"}
+def create_posts(post : Post, db: Session = Depends(get_db)):
+    # new_post = models.Post(title = post.title, content = post.content, published = post.published)
+    new_post = models.Post(**post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return {"data": new_post}
+
+@app.get("/posts/{id}")
+def get_post(id: str, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    # print(post)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    return {"post_detail": post}
+
+@app.delete("/posts/{id}")
+def delete_post(id: str, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
+
+    if post.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} not found")
+    
+    post.delete(synchronize_session=False)
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: str, post:Post, db: Session = Depends(get_db)):
+    update_post = post.model_dump()
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    post = post_query.first()
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} not found")
+    
+    post_query.update(update_post, synchronize_session=False)
+    db.commit()
+
+    return {"data": post_query.first()}
