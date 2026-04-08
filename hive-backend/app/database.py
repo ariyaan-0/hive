2a1e65dfac55c840ca625fe131865f44201c65bd
Md Rezaul Hossain <str.rezaul@gmail.com>
2026-04-08 20:33:50 +0600

    refector env variables

--- hive-backend/app/database.py
+++ hive-backend/app/database.py
@@ -1,15 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from .config import settings
 
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-URL_DATABASE = os.getenv("URL_DATABASE")
-
-engine = create_engine(URL_DATABASE)
+engine = create_engine(settings.url_database)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- hive-backend/app/oauth2.py
+++ hive-backend/app/oauth2.py
@@ -1,29 +1,22 @@
-import os
 from jose import JWTError, jwt
 from datetime import datetime, timedelta, timezone
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-from dotenv import load_dotenv
-
+from .config import settings
 from . import schemas, database, models
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
-load_dotenv()
-
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
 
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.access_token_expire_minutes)
     to_encode.update({"exp": expire})
 
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
 
     return encoded_jwt
 
@@ -32,7 +25,7 @@ def create_access_token(data: dict):
 def verify_access_token(token: str, credentials_exception):
 
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
         id: str = payload.get("user_id")
         if id is None:
             raise credentials_exception
